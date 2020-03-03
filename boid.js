fa39a6c5b704a67666e86858f6d6272a609c6025
matheus nunes <matheusnunes@localhost.localdomain>
2020-03-03 16:38:40 -0300

    teste

---
+++ boid.js
@@ -0,0 +1,175 @@
+// Flocking
+// Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/124-flocking-boids.html
+// https://youtu.be/mhjuuHl6qHM
+// https://editor.p5js.org/codingtrain/sketches/ry4XZ8OkN
+
+class Boid {
+  constructor(highLight) {
+    this.position = createVector(random(width), random(height));
+    this.velocity = p5.Vector.random2D();
+    this.velocity.setMag(random(2, 4));
+    this.acceleration = createVector();
+    this.pointDisp = createVector();
+    this.maxForce = 1;
+    this.maxSpeed = 4;
+    this.highLight = highLight;
+  }
+
+  edges() {
+    if (this.position.x > width) {
+      this.position.x = 0;
+    } else if (this.position.x < 0) {
+      this.position.x = width;
+    }
+    if (this.position.y > height) {
+      this.position.y = 0;
+    } else if (this.position.y < 0) {
+      this.position.y = height;
+    }
+  }
+
+  align(boids) {
+    let perceptionRadius = 50;
+    let steering = createVector();
+    let total = 0;
+
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other != this && d < perceptionRadius) {
+        steering.add(other.velocity);
+        total++;
+        if(connectAll || (this.highLight && highLightConections))
+          this.lineBoids(this, other);
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce);
+    }
+    return steering;
+  }
+
+  separation(boids) {
+    let perceptionRadius = 50;
+    let steering = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other != this && d < perceptionRadius) {
+        let diff = p5.Vector.sub(this.position, other.position);
+        diff.div(d * d);
+        steering.add(diff);
+        total++;
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce);
+    }
+    return steering;
+  }
+
+  cohesion(boids) {
+    let perceptionRadius = 100;
+    let steering = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other != this && d < perceptionRadius) {
+        steering.add(other.position);
+        total++;
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.sub(this.position);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce);
+    }
+    
+    return steering;
+  }
+
+  flock(boids) {
+    let alignment = this.align(boids);
+    let cohesion = this.cohesion(boids);
+    let separation = this.separation(boids);
+
+    alignment.mult(alignSlider.value());
+    cohesion.mult(cohesionSlider.value());
+    separation.mult(separationSlider.value());
+
+    this.acceleration.add(alignment);
+    this.acceleration.add(cohesion);
+    this.acceleration.add(separation);
+
+    if(this.highLight){
+      let pt = createVector(this.velocity.x, this.velocity.y, this.velocity.z);
+      pt.mult(15);
+      pt.add(this.position);
+      this.pointDisp = pt;
+    }
+  }
+
+  update() {
+    this.position.add(this.velocity);
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.acceleration.mult(0);
+  }
+
+  show() {
+    if(ncage){
+      image(ncageIMG, this.position.x - 20, this.position.y - 25);
+    }
+    else if(this.highLight && highLightControll){
+      strokeWeight(2);
+      stroke(0,255 , 255);
+      line(this.position.x, this.position.y, this.pointDisp.x, this.pointDisp.y);
+
+      strokeWeight(8);
+      stroke(0, 255, 0);
+      point(this.position.x, this.position.y);
+      
+      stroke(255,0,0);
+      strokeWeight(2);
+      noFill();
+      circle(this.position.x, this.position.y, 100)
+      strokeWeight(8);
+    }
+    else{
+        strokeWeight(8);
+        stroke(255);
+        if(showBoids)
+          point(this.position.x, this.position.y);
+    }
+  }
+
+  lineBoids(boidA, boidB){
+    stroke(0,0,255);
+    strokeWeight(2);
+    line(boidA.position.x, boidA.position.y, boidB.position.x, boidB.position.y);
+  }
+
+}
---
+++ sketch.js
@@ -0,0 +1,136 @@
+// Flocking
+// Daniel Shiffman
+// https://thecodingtrain.com
+
+// https://thecodingtrain.com/CodingChallenges/124-flocking-boids.html
+// https://youtu.be/mhjuuHl6qHM
+// https://editor.p5js.org/codingtrain/sketches/ry4XZ8OkN
+
+//boids
+const flock = [];
+
+//???????????????????????
+var ncage = false; //////// <<<-------
+var ncageIMG;
+var countFundoNcage = 0;
+var fundoAt;
+
+var fundoNcage = [];
+var fundo = true;
+var nfundos = 3;
+//?????????????????????
+
+//sliders
+let alignSlider, cohesionSlider, separationSlider;
+let marginSliderLeft = 900;
+let marginSlidertop = 100;
+
+//flags de efeitos
+var highLightControll = false; 
+var connectAll = false;
+var showBoids = true;
+var highLightConections = false;
+
+//Botões de controle
+var buttonToggleHighLight;
+var buttonConnectAll;
+var buttonShowBoids;
+var buttonHighLightConections;
+
+function preload(){
+  ncageIMG = loadImage('NCage.png');
+
+  fundoNcage[0] = loadImage('fundoNcage1.jpg');
+  fundoNcage[1] = loadImage('fundoNcage2.jpg');
+  fundoNcage[2] = loadImage('fundoNcage3.jpg');
+}
+
+function setup() {
+
+  createCanvas(1100, 600);
+  alignSlider = createSlider(0, 2, 1, 0.1);
+  cohesionSlider = createSlider(0, 2, 1, 0.1);
+  separationSlider = createSlider(0, 2, 1, 0.1);
+  
+  changeNcage();
+
+  alignSlider.position(marginSliderLeft, marginSlidertop + 0)
+  cohesionSlider.position(marginSliderLeft, marginSlidertop +100)
+  separationSlider.position(marginSliderLeft, marginSlidertop +200)
+
+  for (let i = 0; i < 500; i++) {
+    if(i == 199)
+      flock.push(new Boid(true));
+    else
+      flock.push(new Boid(false));
+  }
+
+  buttonToggleHighLight = createButton('High Light: OFF');
+  buttonToggleHighLight.position(19, 19);
+  buttonToggleHighLight.mousePressed(()=>{toggleFunctions('highLight')});
+  
+  buttonHighLightConections = createButton('High Light interações: ON');
+  buttonHighLightConections.position(270, 19);
+  buttonHighLightConections.mousePressed(()=>{toggleFunctions('highLightConections')});
+
+  buttonConnectAll = createButton('Conectar Todos: OFF');
+  buttonConnectAll.position(19, 570);
+  buttonConnectAll.mousePressed(()=>{toggleFunctions('connectAll')});
+  
+  buttonShowBoids = createButton('Ver Boids: ON');
+  buttonShowBoids.position(270, 570);
+  buttonShowBoids.mousePressed(()=>{toggleFunctions('showBoids')});
+
+}
+
+function draw() {
+  if(ncage && fundo){
+    image(fundoAt, 0, 0);
+  }
+  else if(fundo)
+    //background(51);
+
+    console.log("lala")
+
+  for (let boid of flock){
+    boid.show();
+    boid.edges();
+    boid.flock(flock);
+    boid.update();
+  }
+  
+    
+  textSize(25);
+  fill(0, 100, 255);
+  strokeWeight(3)
+  stroke(0,0,0);
+  text("Alinhamento", marginSliderLeft, 70);
+  text("Coesão", marginSliderLeft, 170);
+  text("Separação", marginSliderLeft, 270);
+}
+
+function toggleFunctions(effect){
+  switch(effect){
+    case 'highLight': 
+      (highLightControll)? (buttonToggleHighLight.html("High Light: OFF")) : (buttonToggleHighLight.html("High Light: ON"));
+      highLightControll = !highLightControll;
+    break;
+    case 'connectAll':
+      (connectAll)? (buttonConnectAll.html("Conectar Todos: OFF")) : (buttonConnectAll.html("Conectar Todos: ON"));
+      connectAll = !connectAll;
+    break;
+    case 'showBoids':
+      (showBoids)? (buttonShowBoids.html("Ver Boids: OFF")) : (buttonShowBoids.html("Ver Boids: ON"));
+      showBoids = !showBoids;
+    break;
+    case 'highLightConections':
+      (highLightConections)? (buttonHighLightConections.html("High Light interações: OFF")) : (buttonHighLightConections.html("High Light interações: ON"));
+      highLightConections = !highLightConections;
+    break;
+  }
+}
+
+function changeNcage(){ 
+  fundoAt = fundoNcage[(countFundoNcage ++) % nfundos]
+  setTimeout(()=>{changeNcage()}, 4000);
+} 
